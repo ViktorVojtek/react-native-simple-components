fd921df71218780deeb3b724107823d3bbc0f3c0
Viktor Vojtek <viktorvojtek@macbookpro.lan>
2025-03-27 21:40:32 +0100

    TouchableOpacity component exposed to package

--- index.ts
+++ index.ts
@@ -1,20 +1,21 @@
-export { default as Button } from "./src/components/Button";
-export type { ButtonProps } from "./src/components/Button";
-export { default as Center } from "./src/components/Center";
-export { default as Circle } from "./src/components/Circle";
-export { default as Checkbox } from "./src/components/Checkbox";
-export { default as Divider } from "./src/components/Divider";
-export { default as Image } from "./src/components/Image";
-export { default as ImageBackground } from "./src/components/ImageBackground";
-export { default as Pressable } from "./src/components/Pressable";
-export { default as ScrollView } from "./src/components/ScrollView";
-export { default as Spinner } from "./src/components/Spinner";
-export { default as Stack, StackY, StackX } from "./src/components/Stack";
-export { default as Text } from "./src/components/Text";
-export { default as TextInput } from "./src/components/TextInput";
+export { default as Button } from './src/components/Button';
+export type { ButtonProps } from './src/components/Button';
+export { default as Center } from './src/components/Center';
+export { default as Circle } from './src/components/Circle';
+export { default as Checkbox } from './src/components/Checkbox';
+export { default as Divider } from './src/components/Divider';
+export { default as Image } from './src/components/Image';
+export { default as ImageBackground } from './src/components/ImageBackground';
+export { default as Pressable } from './src/components/Pressable';
+export { default as ScrollView } from './src/components/ScrollView';
+export { default as Spinner } from './src/components/Spinner';
+export { default as Stack, StackY, StackX } from './src/components/Stack';
+export { default as Text } from './src/components/Text';
+export { default as TextInput } from './src/components/TextInput';
+export { default as TouchableOpacity } from './src/components/TouchableOpacity';
 export type {
   ViewStyleProps,
   MarginProps,
   PaddingProps,
-} from "./src/utils/mapShortStyleProps";
-export { default as ThemeProvider, useTheme } from "./src/ThemeProvider";
+} from './src/utils/mapShortStyleProps';
+export { default as ThemeProvider, useTheme } from './src/ThemeProvider';
